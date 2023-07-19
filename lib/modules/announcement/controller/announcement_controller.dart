fa0e0d46a2e2b7137c375bcd1cba8ab3a246381a
Vannak1111 <vannak251020@gmail.com>
2023-07-19 13:30:10 +0700

    gif

--- lib/modules/announcement/controller/announcement_controller.dart
+++ lib/modules/announcement/controller/announcement_controller.dart
@@ -3,7 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:school/config/url.dart';
 
+import '../../../screens/widgets/exceptions.dart';
+import '../models/announcment_model.dart';
+
 class AnnouncementController extends GetxController {
+  final recAnnouncementList = <Datum>[].obs;
+  final isFirstLoading = true.obs;
+  final isMoreLoading = true.obs;
+  final empty = 0.obs;
+  Future fetchAnnouncement({String pageNo = '1'}) async {
+    Map<String, String> parameters = {
+      'page': pageNo,
+    };
+    try {
+      String fullUrl = '$baseUrlSchool' + 'api/getannouncementlist';
+      await Dio(BaseOptions(
+        receiveDataWhenStatusError: true,
+        connectTimeout: 15000,
+        receiveTimeout: 15000,
+      )).get(fullUrl, queryParameters: parameters).then((value) {
+        AnnouncementDb announcementListDb = AnnouncementDb.fromMap(value.data);
+        return announcementListDb;
+      });
+    } on DioError catch (e) {
+      final errorMessage = DioExceptions.fromDioError(e).toString();
+      return errorMessage;
+    }
+  }
+
   void countAnnouncement(int id) async {
     try {
       var response = await Dio(BaseOptions(headers: {
